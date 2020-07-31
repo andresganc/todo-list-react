d5c7b4840842fb62f4008cd490517e0506e8fee8
Andres Giraldo <andresganc@gmail.com>
2020-07-31 15:45:16 -0500

    Mod: ReduxStore

--- redux-js/src/App.js
+++ redux-js/src/App.js
@@ -1,24 +1,14 @@
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Componentes
+import ReduxStore from 'views/1-basico/1-ReduxStore';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReduxStore />        
     </div>
   );
 }
---
+++ redux-js/src/views/1-basico/Main.jsx
@@ -0,0 +1,15 @@
+
+import React, { Fragment } from 'react';
+
+// Funciones
+import ReduxStore from 'views/1-basico/';
+
+function Main() {
+    return(
+        <Fragment>
+            
+        </Fragment>
+    );
+}
+
+export default Main;
